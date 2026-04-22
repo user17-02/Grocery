c3791a2682108dbb6453a338a73ca94fed3cc2a8
user17-02 <jerosha78@gmail.com>
2026-04-22 15:13:37 +0530

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://farm-97ds.onrender.com/',
+    baseURL: 'https://farm-97ds.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
